c3f975794096a71ae870a50e45e22d6b9e01a2c9
kylie <cooper.kylie1077@gmail.com>
2024-10-09 01:40:50 -0500

    -Init

--- src/main/java/com/dragn0007/dragnlivestock/LivestockOverhaul.java
+++ src/main/java/com/dragn0007/dragnlivestock/LivestockOverhaul.java
@@ -16,7 +16,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 import software.bernie.geckolib3.GeckoLib;
