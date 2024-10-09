28bacbc252cbdc3065081165696857d411af6e47
ffxvfan <ffxvfan@gmail.com>
2024-10-09 00:58:58 -0600

    screen fixes

--- src/main/java/com/dragn0007/dragnlivestock/entities/unicorn/OverworldUnicorn.java
+++ src/main/java/com/dragn0007/dragnlivestock/entities/unicorn/OverworldUnicorn.java
@@ -55,7 +55,6 @@ public class OverworldUnicorn extends OHorse implements IAnimatable, Chestable, 
 	public OverworldUnicorn(EntityType<? extends OverworldUnicorn> type, Level level) {
 		super(type, level);
 		this.noCulling = true;
-		this.updateInventory();
 		this.xpReward = 50;
 	}
 
@@ -63,25 +62,29 @@ public class OverworldUnicorn extends OHorse implements IAnimatable, Chestable, 
 		return Mob.createMobAttributes()
 				.add(Attributes.JUMP_STRENGTH)
 				.add(Attributes.MAX_HEALTH, 80.0D)
-				.add(Attributes.MOVEMENT_SPEED, (double)0.250F)
+				.add(Attributes.MOVEMENT_SPEED, 0.250F)
 				.add(Attributes.ATTACK_DAMAGE, 4D)
 				.add(Attributes.ATTACK_KNOCKBACK, 2F);
 	}
 
+	@Override
 	protected void randomizeAttributes() {
-		this.getAttribute(Attributes.MAX_HEALTH).setBaseValue((double)this.generateRandomMaxHealth());
+		this.getAttribute(Attributes.MAX_HEALTH).setBaseValue(this.generateRandomMaxHealth());
 		this.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(this.generateRandomSpeed());
 		this.getAttribute(Attributes.JUMP_STRENGTH).setBaseValue(this.generateRandomJumpStrength());
 	}
 
+	@Override
 	public float generateRandomMaxHealth() {
 		return 24.0F + (float)this.random.nextInt(8) + (float)this.random.nextInt(9);
 	}
 
+	@Override
 	public double generateRandomJumpStrength() {
 		return (double)0.8F + this.random.nextDouble() * 0.3D + this.random.nextDouble() * 0.3D + this.random.nextDouble() * 0.3D;
 	}
 
+	@Override
 	public double generateRandomSpeed() {
 		return ((double)0.65F + this.random.nextDouble() * 0.3D + this.random.nextDouble() * 0.3D + this.random.nextDouble() * 0.3D) * 0.25D;
 	}
@@ -109,66 +112,13 @@ public class OverworldUnicorn extends OHorse implements IAnimatable, Chestable, 
 	}
 
 	@Override
-	public boolean canBeRiddenInWater(Entity rider) {
-		return false;
-	}
-
-	protected <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
-		double movementSpeed = getAttributeValue(Attributes.MOVEMENT_SPEED);
-		double animationSpeed = Math.max(0.1, movementSpeed);
-		double currentSpeed = this.getDeltaMovement().lengthSqr();
-		double speedThreshold = 0.02;
-
-		if (isJumping() || !this.isOnGround()) {
-			event.getController().setAnimation(
-					new AnimationBuilder().addAnimation("jump", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-
-			event.getController().setAnimationSpeed(1.0);
-
-		} else if (event.isMoving()) {
-			if (currentSpeed > speedThreshold || isAggressive() || isSprinting() || isSwimming()) {
-				event.getController().setAnimation(new AnimationBuilder().addAnimation("run", ILoopType.EDefaultLoopTypes.LOOP));
-				event.getController().setAnimationSpeed(Math.max(0.1, 0.8 * event.getController().getAnimationSpeed() + animationSpeed));
-			} else {
-				event.getController().setAnimation(new AnimationBuilder().addAnimation("walk", ILoopType.EDefaultLoopTypes.LOOP));
-				event.getController().setAnimationSpeed(Math.max(0.1, 0.7 * event.getController().getAnimationSpeed() + animationSpeed));
-			}
-		} else {
-			if (isVehicle()) {
-				event.getController().setAnimation(new AnimationBuilder().addAnimation("idle", ILoopType.EDefaultLoopTypes.LOOP));
-			} else {
-				event.getController().setAnimation(new AnimationBuilder().addAnimation("idle3", ILoopType.EDefaultLoopTypes.LOOP));
-			}
-			event.getController().setAnimationSpeed(1.0);
-		}
-
-		return PlayState.CONTINUE;
-	}
-
-	protected PlayState attackPredicate(AnimationEvent event) {
-		if (this.swinging && event.getController().getAnimationState().equals(AnimationState.Stopped)) {
-			event.getController().markNeedsReload();
-			event.getController().setAnimation(new AnimationBuilder().addAnimation("attack", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-			this.swinging = false;
-		}
-
-		return PlayState.CONTINUE;
-	}
-
-	@Override
-	public void registerControllers (AnimationData data){
-		data.addAnimationController(new AnimationController(this, "controller", 5, this::predicate));
-		data.addAnimationController(new AnimationController(this, "attackController", 1, this::attackPredicate));
-	}
-
 	public void aiStep() {
 		super.aiStep();
-
 		this.level.addParticle(ParticleTypes.MYCELIUM, this.getRandomX(0.6D), this.getRandomY(), this.getRandomZ(0.6D), 0.0D, 0.0D, 0.0D);
 	}
 
 	@Override
-	public boolean causeFallDamage(float p_148875_, float p_148876_, DamageSource p_148877_) {
+	public boolean causeFallDamage(float f1, float f2, DamageSource damageSource) {
 		return false;
 	}
 
@@ -208,181 +158,19 @@ public class OverworldUnicorn extends OHorse implements IAnimatable, Chestable, 
 		}
 	}
 
-//	public InteractionResult mobInteract(Player player, InteractionHand hand) {
-//		ItemStack itemstack = player.getItemInHand(hand);
-//
-//		if (this.isFood(itemstack) && this.isTamed()) {
-//			if (this.getHealth() < this.getMaxHealth()) {
-//				this.usePlayerItem(player, hand, itemstack);
-//				this.heal(itemstack.getFoodProperties(this).getNutrition());
-//				this.gameEvent(GameEvent.MOB_INTERACT, this.eyeBlockPosition());
-//				return InteractionResult.sidedSuccess(this.level.isClientSide);
-//			}
-//		}
-//
-//		if (this.isFood(itemstack) && this.isTamed()) {
-//			if (this.canFallInLove() && !this.level.isClientSide) {
-//				this.usePlayerItem(player, hand, itemstack);
-//				this.setInLove(player);
-//				this.gameEvent(GameEvent.MOB_INTERACT, this.eyeBlockPosition());
-//				return InteractionResult.SUCCESS;
-//			}
-//		}
-//
-//		if (this.isBaby()) {
-//			return super.mobInteract(player, hand);
-//		}
-//
-//		if (!this.isTamed() && this.isFood(itemstack)) {
-//			return this.fedFood(player, itemstack);
-//		}
-//
-//		if (this.isTamed() && player.isSecondaryUseActive()) {
-//			this.openInventory(player);
-//			return InteractionResult.sidedSuccess(this.level.isClientSide);
-//		}
-//
-//		if (this.isVehicle()) {
-//			return super.mobInteract(player, hand);
-//		}
-//
-//		if (!itemstack.isEmpty()) {
-//			if (!this.isTamed()) {
-//				this.makeMad();
-//				return InteractionResult.sidedSuccess(this.level.isClientSide);
-//			}
-//
-//			if (itemstack.is(Items.SADDLE) && !this.isSaddled() && this.isSaddleable()) {
-//				this.setSaddled(true);
-//				this.updateInventory();
-//				if (!player.getAbilities().instabuild) {
-//					itemstack.shrink(1);
-//				}
-//				return InteractionResult.sidedSuccess(this.level.isClientSide);
-//			}
-//
-//			if (!this.level.isClientSide) {
-//				if (player.isShiftKeyDown()) {
-//					NetworkHooks.openGui((ServerPlayer) player, new SimpleMenuProvider((containerId, inventory, serverPlayer) -> {
-//						return new OHorseMenu(containerId, inventory, this.inventory, this);
-//					}, this.getDisplayName()), (data) -> {
-//						data.writeInt(this.getInventorySize());
-//						data.writeInt(this.getId());
-//					});
-//					return InteractionResult.SUCCESS;
-//				}
-//
-//				InteractionResult interactionresult = itemstack.interactLivingEntity(player, this, hand);
-//				if (interactionresult.consumesAction()) {
-//					return interactionresult;
-//				}
-//			}
-//		}
-//
-//		this.doPlayerRide(player);
-//		return InteractionResult.sidedSuccess(this.level.isClientSide);
-//	}
-
-	protected void playGallopSound(SoundType p_30709_) {
-		super.playGallopSound(p_30709_);
-		if (this.random.nextInt(10) == 0) {
-			this.playSound(SoundEvents.HORSE_BREATHE, p_30709_.getVolume() * 0.6F, p_30709_.getPitch());
-		}
-
-		ItemStack stack = this.inventory.getItem(1);
-		if (isArmor(stack)) stack.onHorseArmorTick(level, this);
-	}
-
 	@Override
-	public boolean isSaddleable() {
-		return this.isAlive() && !this.isBaby();
-	}
-
-	@Override
-	public void equipSaddle(@Nullable SoundSource soundSource) {
-		this.inventory.setItem(0, new ItemStack(Items.SADDLE));
-		if (soundSource != null) {
-			this.level.playSound(null, this, SoundEvents.HORSE_SADDLE, soundSource, 0.5f, 1.0f);
-		}
-	}
-
-	@Override
-	public boolean isSaddled() {
-		return this.entityData.get(SADDLED);
-	}
-
-	protected void setSaddled(boolean saddled) {
-		this.entityData.set(SADDLED, saddled);
-	}
-
-	@Override
-	protected void dropEquipment() {
-		if(!this.level.isClientSide) {
-			super.dropEquipment();
-			if(this.isChested()) {
-				this.spawnAtLocation(Items.CHEST);
-			}
-			Containers.dropContents(this.level, this, this.inventory);
-		}
-	}
-
-	protected SoundEvent getAmbientSound() {
-		super.getAmbientSound();
-		return SoundEvents.HORSE_AMBIENT;
-	}
-
-	protected SoundEvent getDeathSound() {
-		super.getDeathSound();
-		return SoundEvents.HORSE_DEATH;
-	}
-
-	@Nullable
-	protected SoundEvent getEatingSound() {
-		return SoundEvents.HORSE_EAT;
-	}
-
-	protected SoundEvent getHurtSound(DamageSource p_30720_) {
-		super.getHurtSound(p_30720_);
-		return SoundEvents.HORSE_HURT;
-	}
-
-	protected SoundEvent getAngrySound() {
-		super.getAngrySound();
-		return SoundEvents.HORSE_ANGRY;
-	}
-
-	// Generates the base texture
-	public static final EntityDataAccessor<ResourceLocation> VARIANT_TEXTURE = SynchedEntityData.defineId(OverworldUnicorn.class, LivestockOverhaul.RESOURCE_LOCATION);
-	public static final EntityDataAccessor<ResourceLocation> OVERLAY_TEXTURE = SynchedEntityData.defineId(OverworldUnicorn.class, LivestockOverhaul.RESOURCE_LOCATION);
-
-	public ResourceLocation getTextureLocation() {
-		return this.entityData.get(VARIANT_TEXTURE);
-//		return OHorseModel.Variant.variantFromOrdinal(getVariant()).resourceLocation;
-	}
-	public ResourceLocation getOverlayLocation() {
-		return this.entityData.get(OVERLAY_TEXTURE);
-//		return OHorseMarkingLayer.Overlay.overlayFromOrdinal(getOverlayVariant()).resourceLocation;
-	}
-
-	protected static final EntityDataAccessor<Integer> VARIANT = SynchedEntityData.defineId(OverworldUnicorn.class, EntityDataSerializers.INT);
-	protected static final EntityDataAccessor<Integer> OVERLAY = SynchedEntityData.defineId(OverworldUnicorn.class, EntityDataSerializers.INT);
-
-	public int getVariant() {
-		return this.entityData.get(VARIANT);
-	}
-	public int getOverlayVariant() {
-		return this.entityData.get(OVERLAY);
-	}
-
 	public void setVariant(int variant) {
 		this.entityData.set(VARIANT_TEXTURE, OverworldUnicornModel.Variant.variantFromOrdinal(variant).resourceLocation);
 		this.entityData.set(VARIANT, variant);
 	}
+
+	@Override
 	public void setOverlayVariant(int variant) {
 		this.entityData.set(OVERLAY_TEXTURE, OverworldUnicornHornLayer.Overlay.overlayFromOrdinal(variant).resourceLocation);
 		this.entityData.set(OVERLAY, variant);
 	}
 
+	@Override
 	public void setVariantTexture(String variant) {
 		ResourceLocation resourceLocation = ResourceLocation.tryParse(variant);
 		if (resourceLocation == null) {
@@ -390,99 +178,14 @@ public class OverworldUnicorn extends OHorse implements IAnimatable, Chestable, 
 		}
 		this.entityData.set(VARIANT_TEXTURE, resourceLocation);
 	}
+
+	@Override
 	public void setOverlayVariantTexture(String variant) {
 		ResourceLocation resourceLocation = ResourceLocation.tryParse(variant);
 		if (resourceLocation == null) {
 			resourceLocation = OverworldUnicornHornLayer.Overlay.NONE.resourceLocation;
 		}
 		this.entityData.set(OVERLAY_TEXTURE, resourceLocation);
-	}
-
-	@Override
-	public void readAdditionalSaveData(CompoundTag tag) {
-		super.readAdditionalSaveData(tag);
-
-		if (tag.contains("Variant")) {
-			setVariant(tag.getInt("Variant"));
-		}
-
-		if (tag.contains("Overlay")) {
-			setOverlayVariant(tag.getInt("Overlay"));
-		}
-
-		if (tag.contains("Variant_Texture")) {
-			setVariantTexture(tag.getString("Variant_Texture"));
-		}
-
-		if (tag.contains("Overlay_Texture")) {
-			setOverlayVariantTexture(tag.getString("Overlay_Texture"));
-		}
-
-		if (tag.contains("Chested")) {
-			this.setChested(tag.getBoolean("Chested"));
-		}
-
-		if (tag.contains("Saddled")) {
-			this.setSaddled(tag.getBoolean("Saddled"));
-		}
-
-		if (tag.contains("ArmorItem", 10)) {
-			ItemStack itemstack = ItemStack.of(tag.getCompound("ArmorItem"));
-			if (!itemstack.isEmpty() && this.isArmor(itemstack)) {
-				this.inventory.setItem(1, itemstack);
-			}
-		}
-
-		this.setTamed(tag.getBoolean("Tame"));
-		UUID uuid;
-		if (tag.hasUUID("Owner")) {
-			uuid = tag.getUUID("Owner");
-		} else {
-			String s = tag.getString("Owner");
-			uuid = OldUsersConverter.convertMobOwnerIfNecessary(this.getServer(), s);
-		}
-
-		if (uuid != null) {
-			this.setOwnerUUID(uuid);
-		}
-
-		if (tag.contains("SaddleItem", 10)) {
-			ItemStack itemstack = ItemStack.of(tag.getCompound("SaddleItem"));
-			if (itemstack.is(Items.SADDLE)) {
-				this.inventory.setItem(0, itemstack);
-			}
-		}
-
-		this.updateContainerEquipment();
-	}
-
-	@Override
-	public void addAdditionalSaveData(CompoundTag tag) {
-		super.addAdditionalSaveData(tag);
-		tag.putInt("Variant", getVariant());
-
-		tag.putInt("Overlay", getOverlayVariant());
-
-		tag.putString("Variant_Texture", getTextureLocation().toString());
-
-		tag.putString("Overlay_Texture", getOverlayLocation().toString());
-
-		tag.putBoolean("Chested", this.isChested());
-
-		tag.putBoolean("Saddled", this.isSaddled());
-
-		tag.putBoolean("Tame", this.isTamed());
-		if (this.getOwnerUUID() != null) {
-			tag.putUUID("Owner", this.getOwnerUUID());
-		}
-
-		if (!this.inventory.getItem(0).isEmpty()) {
-			tag.put("SaddleItem", this.inventory.getItem(0).save(new CompoundTag()));
-		}
-
-		if (!this.inventory.getItem(1).isEmpty()) {
-			tag.put("ArmorItem", this.inventory.getItem(1).save(new CompoundTag()));
-		}
 	}
 
 	@Override
@@ -500,27 +203,6 @@ public class OverworldUnicorn extends OHorse implements IAnimatable, Chestable, 
 	}
 
 	@Override
-	protected void defineSynchedData() {
-		super.defineSynchedData();
-		this.entityData.define(VARIANT, 0);
-		this.entityData.define(OVERLAY, 0);
-		this.entityData.define(VARIANT_TEXTURE, OverworldUnicornModel.Variant.OVERWORLD.resourceLocation);
-		this.entityData.define(OVERLAY_TEXTURE, OverworldUnicornHornLayer.Overlay.NONE.resourceLocation);
-		this.entityData.define(CHESTED, false);
-		this.entityData.define(SADDLED, false);
-	}
-
-	protected void updateContainerEquipment() {
-		if (!this.level.isClientSide) {
-			super.updateContainerEquipment();
-			this.setDropChance(EquipmentSlot.CHEST, 0.0F);
-		}
-	}
-
-	protected boolean canParent() {
-		return !this.isVehicle() && !this.isPassenger() && this.isTamed() && !this.isBaby() && this.getHealth() >= this.getMaxHealth() && this.isInLove();
-	}
-
 	public boolean canMate(Animal animal) {
 		return false;
 	}
@@ -530,35 +212,4 @@ public class OverworldUnicorn extends OHorse implements IAnimatable, Chestable, 
 		return null;
 	}
 
-	@Override
-	public boolean isChestable() {
-		return this.isAlive() && !this.isBaby();
-	}
-
-	@Override
-	public void equipChest(@Nullable SoundSource soundSource) {
-		if(soundSource != null) {
-			this.level.playSound(null, this, SoundEvents.MULE_CHEST, soundSource, 0.5f, 1f);
-		}
-	}
-
-	@Override
-	public boolean isChested() {
-		return this.entityData.get(CHESTED);
-	}
-
-	protected void setChested(boolean chested) {
-		this.entityData.set(CHESTED, chested);
-	}
-
-	@Override
-	public void containerChanged(Container container) {
-		boolean flag = this.isSaddled();
-		if (!this.level.isClientSide) {
-			this.setSaddled(!this.inventory.getItem(0).isEmpty());
-		}
-		if (this.tickCount > 20 && !flag && this.isSaddleable()) {
-			this.playSound(SoundEvents.HORSE_SADDLE, 0.5f, 1f);
-		}
-	}
 }
--- src/main/java/com/dragn0007/dragnlivestock/gui/LOMenuTypes.java
+++ src/main/java/com/dragn0007/dragnlivestock/gui/LOMenuTypes.java
@@ -1,16 +1,22 @@
 package com.dragn0007.dragnlivestock.gui;
 
 import com.dragn0007.dragnlivestock.LivestockOverhaul;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.network.IContainerFactory;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class LOMenuTypes {
     public static final DeferredRegister<MenuType<?>> MENU_TYPES = DeferredRegister.create(ForgeRegistries.CONTAINERS, LivestockOverhaul.MODID);
+    public static final RegistryObject<MenuType<OHorseMenu>> O_HORSE_MENU = registerMenuType("ohorse_menu", OHorseMenu::new);
 
-    public static final RegistryObject<MenuType<OHorseMenu>> O_HORSE_MENU = MENU_TYPES.register("o_horse_menu", () -> new MenuType<>(OHorseMenu.create()));
+    private static <T extends AbstractContainerMenu>RegistryObject<MenuType<T>> registerMenuType(String name, IContainerFactory<T> factory) {
+        return MENU_TYPES.register(name, () -> IForgeMenuType.create(factory));
+    }
 
     public static void register(IEventBus eventBus) {
         MENU_TYPES.register(eventBus);
